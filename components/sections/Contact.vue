372bc78f2e3d553d74c7a548ce317efd98923d15
Gio Rodriguez <giorodriguez@ninepinetech.com>
2020-12-28 00:37:46 +0800

    Updated form attributes and headers for contact form

--- components/sections/Contact.vue
+++ components/sections/Contact.vue
@@ -6,7 +6,7 @@
         <div cols="12" class="contactFormHeader">
           <p>Get in touch with Us</p>
         </div>
-        <b-form name="inquiries" @submit="sendInquiry" data-netlify="true" action="/">
+        <b-form name="inquiries" @submit="sendInquiry" data-netlify="true" method="POST" action="/#contact">
           <b-form-group
             label="Name:"
           >
@@ -127,7 +127,7 @@ export default {
     sendInquiry(event) {
       event.preventDefault()
       this.isSubmitting = true
-      this.$axios.post('/', this.encode({ 'form-name': 'inquiries', 'subject': this.subject, ...this.contactForm }), { headers: { 'Content-Type': 'application/x-www-form-urlencoded'  } })
+      this.$axios.post('/#contact', this.encode({ 'form-name': 'inquiries', 'subject': this.subject, ...this.contactForm }), { headers: { 'Accept': 'application/x-www-form-urlencoded', 'Content-Type': 'application/x-www-form-urlencoded'  } })
       .then(() => {
         this.isSubmitting = false
         this.contactFormMessage = { color: 'success', message: 'Message was submitted. We will get back to you on your preferred mode of communication. Thank you for reaching out!' }
