110359fc626bc772400503c6f05796c18c3bd2b1
Gio Rodriguez <giorodriguez@ninepinetech.com>
2020-12-27 20:06:15 +0800

    Contact form to send in netlify

--- components/sections/Contact.vue
+++ components/sections/Contact.vue
@@ -14,6 +14,7 @@
               v-model="contactForm.name"
               type="text"
               placeholder="Your name or your business name"
+              @input="clearMessage"
             ></b-form-input>
           </b-form-group>
           <b-form-group
@@ -21,7 +22,8 @@
           >
             <b-form-input
               v-model="contactForm.email"
-              type="email"
+              type="text"
+              @input="clearMessage"
             ></b-form-input>
           </b-form-group>
           <b-form-group
@@ -30,6 +32,7 @@
             <b-form-input
               v-model="contactForm.contact_no"
               type="text"
+              @input="clearMessage"
             ></b-form-input>
           </b-form-group>
           <b-form-group
@@ -38,9 +41,10 @@
           >
             <b-form-radio-group
               id="radio-group-1"
-              v-model="contactForm.mode_of_communication"
-              :options="mode_of_communications"
+              v-model="contactForm.preferred_mode_of_communication"
+              :options="preferred_mode_of_communications"
               :aria-describedby="ariaDescribedby"
+              @change="clearMessage"
             ></b-form-radio-group>
           </b-form-group>
           <b-form-group
@@ -50,9 +54,16 @@
               v-model="contactForm.message"
               rows="3"
               max-rows="6"
+              @input="clearMessage"
             ></b-form-textarea>
           </b-form-group>
-          <b-button class="sendBtn" pill>Send</b-button>
+          <div class="messageAndButton" :class="[contactFormMessage ? 'space-between' : 'flex-end']">
+            <span v-if="contactFormMessage" :class="'text-'+contactFormMessage.color">{{contactFormMessage.message}}</span>
+            <b-button type="submit" class="sendBtn" pill>
+              <b-icon v-if="isSubmitting" icon="arrow-clockwise" animation="spin"></b-icon>
+              <span v-else>Send</span>
+            </b-button>
+          </div>
         </b-form>
       </b-container>
     </div>
@@ -60,26 +71,62 @@
 </template>
 
 <script>
+import { BIcon, BIconArrowClockwise } from 'bootstrap-vue'
+
 export default {
   name: 'Contact',
+  components: {
+    BIcon,
+    BIconArrowClockwise
+  },
   data() {
     return {
       contactForm: {
         name: '',
         email: '',
         contact_no: '',
-        mode_of_communication: 'Phone',
+        preferred_mode_of_communication: 'Phone',
         message: ''
       },
-      mode_of_communications: [
+      preferred_mode_of_communications: [
         { text: 'Phone', value: 'Phone' },
         { text: 'Email', value: 'Email' }
-      ]
+      ],
+      contactFormMessage: null,
+      isSubmitting: false
     }
   },
   methods: {
+    encode(data) {
+      return Object.keys(data).map(key =>
+        `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+      ).join('&')
+    },
+    clearMessage() {
+      this.contactFormMessage = null
+    },
+    resetFields() {
+      Object.keys(this.contactForm).map(key => {
+        if(key != 'preferred_mode_of_communication') {
+          this.contactForm[key] = ''
+        } else {
+          this.contactForm[key] = 'Phone'
+        }
+      })
+    },
     sendInquiry() {
-
+      this.isSubmitting = true
+      axios.post('/', this.encode({ 'form-name': 'inquiries', ...this.contactForm }), { headers: { 'Content-Type': 'application/x-www-form-urlencoded'  } })
+      .then(() => {
+        this.isSubmitting = false
+        this.contactFormMessage = { color: 'success', message: 'Message was submitted. We will get back to you on your preferred mode of communication. Thank you for reaching out!' }
+        this.resetFields()
+      })
+      .catch(err => {
+        console.log(err)
+        this.isSubmitting = false
+        this.contactFormMessage = { color: 'danger', message: 'There was an error submitting your message.' }
+      })
     }
   }
 }
@@ -120,6 +167,23 @@ export default {
     font-weight: 700;
   }
 
+  .messageAndButton {
+    display: flex;
+  }
+
+  .messageAndButton span {
+    font-size: 13px;
+    font-weight: 600;
+  }
+
+  .flex-end {
+    justify-content: flex-end;
+  }
+
+  .space-between {
+    justify-content: space-between;
+  }
+
   .sendBtn {
     background-color: #f6961e;
     border-color: #f6961e;
@@ -128,7 +192,6 @@ export default {
     font-weight: 700;
     letter-spacing: 0.1em;
     padding: 8px 20px;
-    float: right;
   }
 
   .sendBtn:hover {
