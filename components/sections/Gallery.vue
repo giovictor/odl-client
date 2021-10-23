7c411aaf94adb965aea846c675c6b6efdf004149
Gio Rodriguez <giorodriguez021@gmail.com>
2021-10-23 20:38:47 +0800

    Updated slick settings and stylings/added identifier_field in config for gallery collections

--- components/sections/Gallery.vue
+++ components/sections/Gallery.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="gallery">
     <b-container>
+      <h2>Gallery</h2>
       <div class="gallery-images">
         <vue-slick-carousel v-bind="carousel" v-if="galleryImages.length != 0">
           <div class="gallery-image" v-for="gallery in galleryImages" :key="gallery.image">
@@ -31,13 +32,31 @@ export default {
         infinite: true,
         rows: 2,
         slidesToShow: 3,
-        slidesPerRow: 1,
-        speed: 500,
+        slidesToScroll: 3,
+        speed: 1000,
         autoplay: true,
-        autoplaySpeed: 5000,
+        autoplaySpeed: 3000,
         pauseOnDotsHover: true,
         pauseOnFocus: true,
-        pauseOnHover: true
+        pauseOnHover: true,
+        responsive: [
+          {
+            breakpoint: 991,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              rows: 1,
+              dots: false
+            }
+          }
+        ]
       }
     }
   },
@@ -46,7 +65,7 @@ export default {
   },
   methods: {
     async getGalleryImages() {
-      this.galleryImages = await this.$content('gallery').fetch()
+      this.galleryImages = await this.$content('gallery').sortBy('image').fetch()
     }
   }
 }
@@ -58,14 +77,25 @@ export default {
     height: 600px;
   }
 
+  #gallery h2 {
+    font-weight: 700;
+    font-size: 24px;
+    color: #89b83e;
+    letter-spacing: 0.2em;
+    text-transform: uppercase;
+    margin: 30px 0;
+  }
+
   .gallery-images {
-    padding: 56px 0;
+    padding-bottom: 56px;
   }
 
   .gallery-image img {
     height: 241px;
     width: 100%;
     padding: 5px;
+    object-fit: cover;
+    -o-object-fit: cover;
   }
 
   .slick-prev::before, .slick-next::before {
@@ -75,6 +105,23 @@ export default {
   @media screen and (max-width: 991px) {
     #gallery {
       height: 100%;
+    }
+
+    .gallery-image img {
+      height: 360px;
+    }
+  }
+
+  @media screen and (max-width: 576px) {
+    #gallery {
+      padding-left: 20px;
+      padding-right: 20px;
+    }
+  }
+
+  @media screen and (max-width: 480px) {
+    .gallery-images {
+      padding-bottom: 0;
     }
   }
 </style>
